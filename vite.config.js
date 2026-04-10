8c2fb1f68089b71dea9fe7a86f5badc350de5f40
Elena <113547059+memgn19@users.noreply.github.com>
2026-04-10 14:34:04 +0200

    fixed

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,10 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: process.env.VITE_BASE_PATH || "/portfolio",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    }
+    },
   }
 })
